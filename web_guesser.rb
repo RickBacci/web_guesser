becfebd1fa6d991d3c76c0cddcbbc50df998f83d
Rick Bacci <rickbacci@gmail.com>
2015-05-12 19:19:30 -0600

    extract logic to method

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,15 +5,20 @@ set :number, rand(100)
 
 get '/' do
   guess = params['guess'].to_i
+  message = check_guess(guess)
 
   message =
-    if guess < settings.number
-      settings.number - guess > 5 ? 'Way to low!':'Too low!'
-    elsif guess > settings.number
-      guess - settings.number > 5 ? 'Way to high!':'Too high!'
-    else
-      "You got it right!"
-    end
+
 
   erb :index, :locals => { :number => settings.number, :message => message }
+end
+
+def check_guess(guess)
+  if guess < settings.number
+    settings.number - guess > 5 ? 'Way to low!':'Too low!'
+  elsif guess > settings.number
+    guess - settings.number > 5 ? 'Way to high!':'Too high!'
+  else
+    "You got it right!"
+  end
 end
